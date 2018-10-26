981145b9e4c668551ea8cc9d9ac6c2b895d4e202
Ronan Murphy <r.murphy33@nuigalway.ie>
2018-10-26 15:43:10 +0100

    Final Commit

--- src/test/java/StudentRegTest.java
+++ src/test/java/StudentRegTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//junit tesst class for student registration username
 
 import classes.Student;
 
